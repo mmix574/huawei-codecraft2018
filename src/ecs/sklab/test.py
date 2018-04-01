9185e0c4db4845b992c83a21dfcbfb518960b220
taita2015 <mmix.xia@gmail.com>
2018-04-01 16:32:41 +0800

    0401

--- src/ecs/sklab/test.py
+++ src/ecs/sklab/test.py
@@ -4,17 +4,15 @@ from sklearn.preprocessing import StandardScaler,Normalizer,MinMaxScaler
 
 
 
-N = 6
+N = 3
 
 X_train,Y_train,X_val,Y_val = load_xy(prefix='../data/',argumentation = True,N=N)
 
 
 
-
-
 from sklearn.linear_model import Lasso
 from sklearn.cross_validation import cross_val_score
-clf = LinearRegression()
+from sklearn.preprocessing import normalize
 
 def sample_X(X,i):
     K = X.reshape(X.shape[0],-1,20)
@@ -22,39 +20,75 @@ def sample_X(X,i):
 def sample_Y(Y,i):
     return Y[:,i]
 
-
-sds = StandardScaler()
-
-
-
-# for i in range(Y_train.shape[1]):
-#     # print(sample_X(X_train,i))
-#     # print(sample_Y(Y_train,i))
-#     scores = cross_val_score(clf,sample_X(X_train,i),sample_Y(Y_train,i),cv=10)
-#     print(scores)
-#     # clf.fit(sample_X(X_train,i),sample_Y(Y_train,i))
-#     # fs = clf.score(sample_X(X_train,i),sample_Y(Y_train,i))
-#     # vs = clf.score(sample_X(X_val,i),sample_Y(Y_val,i))
-#     # print(fs,vs)
-
-
-# print(
-# sds.fit_transform(sample_X(X_train,0))
-# )
-
 import numpy as np
 
-from sklearn.preprocessing import normalize
 
-A = np.arange(9).reshape(3,3)
+X_train,X_norm = normalize(X_train,axis=0,return_norm=True)
+# Y_train,Y_norm = normalize(Y_train,axis=0,return_norm=True)
 
-# B = np.arange(9).reshape(3,3)
-# print(Normalizer(norm='l2').fit_transform(A,axis=1)
-# )
+X_val/=X_norm
+# Y_val/=Y_norm
 
-A_transform,norm = normalize(A,axis=0,norm='l2',return_norm=True)
+def score(Y,Y_):
+    Y = np.array(Y)
+    Y_ = np.array(Y_)
+    if(len(np.shape(Y))==1):
+        Y = Y.reshape(-1,1)
+    if(len(np.shape(Y_))==1):
+        Y_ = Y_.reshape(-1,1)
+
+    A = np.power(np.sum((Y-Y_)**2,axis=1),0.5)
+    B = np.power(np.sum((Y)**2,axis=1),0.5) + np.power(np.sum((Y_)**2,axis=1),0.5)
+    # print(1-np.array(A/B))
+    score = 1-np.mean(np.array(A/B))
+    # print('-->',1-np.mean(np.array(A/B)))
+    print(score)
+    return score
 
 
-print(A_transform*norm)
+from sklearn.linear_model import Lasso
+clf = Lasso(fit_intercept=False,alpha=0.9)
+# clf = LinearRegression(fit_intercept=False)
 
+
+for i in range(Y_train.shape[1]):
+    clf.fit(sample_X(X_train,i),sample_Y(Y_train,i))
+    # print(clf.score(sample_X(X_val,i),sample_Y(Y_val,i)))
+    p = clf.predict(sample_X(X_val,i))
+    score(p,sample_Y(Y_val,i))
+    # clf.fit(np.log(sample_X(X_train,i)+1),sample_Y(Y_train,i))
+    # print(clf.score(np.log(sample_X(X_val,i)+1),sample_Y(Y_val,i)))
+
+    # clf.fit(X_train,sample_Y(Y_train,i))
+    # print(clf.score(X_val,sample_Y(Y_val,i)))
+    # clf.fit(np.log(X_train+1),sample_Y(Y_train,i))
+    # print(clf.score(np.log(X_val+1),sample_Y(Y_val,i)))
+
+    print('')
+
+
+
+
+# clf.fit(np.log(X_train+1),Y_train)
+# print(clf.score(np.log(X_val+1),Y_val))
+
+# clf.fit(np.sqrt(X_train+1),Y_train)
+# print(clf.score(np.sqrt(X_val+1),Y_val))
+
+# sds = StandardScaler()
+
+# import numpy as np
+
+
+# A = np.arange(9).reshape(3,3)
+
+# # B = np.arange(9).reshape(3,3)
+# # print(Normalizer(norm='l2').fit_transform(A,axis=1)
+# # )
+
+# A_transform,norm = normalize(A,axis=0,norm='l2',return_norm=True)
+
+# B = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]]) 
+# print(A_transform*norm)
+# print(B/norm)
 
