2a43cbcfa674bab0d07e224b0a75891fadf176f7
taita2015 <mmix.xia@gmail.com>
2018-04-01 10:10:29 +0800

    0401

--- src/ecs/sklab/test.py
+++ src/ecs/sklab/test.py
@@ -1,4 +1,7 @@
 from load_data import load_xy
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler,Normalizer,MinMaxScaler
+
 
 
 N = 6
@@ -6,21 +9,8 @@ N = 6
 X_train,Y_train,X_val,Y_val = load_xy(prefix='../data/',argumentation = True,N=N)
 
 
-from sklearn.linear_model import LinearRegression
-# clf = LinearRegression()
-# clf.fit(X_train,Y_train)
-
-# print(clf.score(X_train,Y_train))
 
 
-# print(clf.score(X_val,Y_val))
-# print(X_train.shape)
-# print(X_val.shape)
-
-
-# from sklearn.ensemble import BaggingRegressor
-# from sklearn.tree import DecisionTreeRegressor
-# clf = BaggingRegressor(base_estimator=DecisionTreeRegressor())
 
 from sklearn.linear_model import Lasso
 from sklearn.cross_validation import cross_val_score
@@ -33,24 +23,38 @@ def sample_Y(Y,i):
     return Y[:,i]
 
 
-for i in range(Y_train.shape[1]):
-    # print(sample_X(X_train,i))
-    # print(sample_Y(Y_train,i))
-    scores = cross_val_score(clf,sample_X(X_train,i),sample_Y(Y_train,i),cv=10)
-    print(scores)
-    # clf.fit(sample_X(X_train,i),sample_Y(Y_train,i))
-    # fs = clf.score(sample_X(X_train,i),sample_Y(Y_train,i))
-    # vs = clf.score(sample_X(X_val,i),sample_Y(Y_val,i))
-    # print(fs,vs)
+sds = StandardScaler()
+
 
 
 # for i in range(Y_train.shape[1]):
-    # clf.fit(X_train[:,i:i+1],Y_train[:,i])
-    # clf.fit(X_val,Y_val[:,i])
-    # print(clf.score(X_train[:,i:i+1],Y_train[:,i]),clf.score(X_val[:,i:i+1],Y_val[:,i]))
-    # print(clf.score())
+#     # print(sample_X(X_train,i))
+#     # print(sample_Y(Y_train,i))
+#     scores = cross_val_score(clf,sample_X(X_train,i),sample_Y(Y_train,i),cv=10)
+#     print(scores)
+#     # clf.fit(sample_X(X_train,i),sample_Y(Y_train,i))
+#     # fs = clf.score(sample_X(X_train,i),sample_Y(Y_train,i))
+#     # vs = clf.score(sample_X(X_val,i),sample_Y(Y_val,i))
+#     # print(fs,vs)
 
 
+# print(
+# sds.fit_transform(sample_X(X_train,0))
+# )
 
+import numpy as np
+
+from sklearn.preprocessing import normalize
+
+A = np.arange(9).reshape(3,3)
+
+# B = np.arange(9).reshape(3,3)
+# print(Normalizer(norm='l2').fit_transform(A,axis=1)
+# )
+
+A_transform,norm = normalize(A,axis=0,norm='l2',return_norm=True)
+
+
+print(A_transform*norm)
 
 
