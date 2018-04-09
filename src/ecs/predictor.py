37a22ffd832cd21d87dc33dc037ed645484c4eaa
taita2015 <mmix.xia@gmail.com>
2018-04-09 16:14:46 +0800

    0409

--- src/ecs/predictor.py
+++ src/ecs/predictor.py
@@ -278,7 +278,7 @@ def corrcoef_supoort_ridge(ecs_logs,flavors_unique,training_start_time,training_
     predict_days = (predict_end_time-predict_start_time).days
 
     N = 1
-    X_train_raw,Y_train_raw,X_test  = resample(ecs_logs,flavors_unique,training_start_time,predict_start_time,frequency='{}d'.format(predict_days),N=N,argumentation=True,outlier_handling=True)
+    X_train_raw,Y_train_raw,X_test  = resample(ecs_logs,flavors_unique,training_start_time,predict_start_time,frequency='{}d'.format(predict_days),N=N,argumentation=True,outlier_handling=False)
     X_train,X_val,Y_train,Y_val = train_test_split(X_train_raw,Y_train_raw,test_size=predict_days-1,align='right')
     corrcoef_of_data = corrcoef(X_train)
     
@@ -379,6 +379,9 @@ def features_ridge(ecs_logs,flavors_unique,training_start_time,training_end_time
         virtual_machine_sum += int(round(p))
     return predict,virtual_machine_sum
 
+
+# add @ 2018-04-09 
+# zero padding feature extraction
 def features_building(ecs_logs,flavors_unique,training_start_time,training_end_time,predict_start_time,predict_end_time):
     predict = {}.fromkeys(flavors_unique)
     for f in flavors_unique:
@@ -387,28 +390,20 @@ def features_building(ecs_logs,flavors_unique,training_start_time,training_end_t
     mapping_index = get_flavors_unique_mapping(flavors_unique)
     predict_days = (predict_end_time-predict_start_time).days
 
-    N = 5
-    X_train_raw,Y_train_raw,X_test  = resample(ecs_logs,flavors_unique,training_start_time,predict_start_time,frequency='{}d'.format(predict_days),N=N,argumentation=True,outlier_handling=False)
-    X_train_raw,Y_train_raw,X_test = normaling(X_train_raw,Y_train_raw,X_test,normalize_method='normalize',norm='l1')
+    N = 1
+    X_train_raw,Y_train_raw,X_test_raw  = resample(ecs_logs,flavors_unique,training_start_time,predict_start_time,frequency='{}d'.format(predict_days),N=N,argumentation=True,outlier_handling=False)
+    X_train_raw,Y_train_raw,X_test_raw = normaling(X_train_raw,Y_train_raw,X_test_raw,normalize_method='normalize',norm='l1')
     corrcoef_of_data = corrcoef(X_train_raw)
-    
-    X_train,X_val,Y_train,Y_val = train_test_split(X_train_raw,Y_train_raw,test_size=predict_days-1,align='right')
 
-    features = []
-    # feature building for each flavors
-    # for i in flavors_unique
-
+    X,y = vstack([X_train_raw,X_test_raw]),Y_train_raw
+    print(shape(X))
+    print(shape(y))
     exit()
-    result = [0 if r<0 else r for r in result]
-    for f in flavors_unique:
-        p = result[mapping_index[f]]
-        predict[f] = int(round(p))
-        virtual_machine_sum += int(round(p))
-    
-    return features
 
 
-# todo
+    X_train_flavors_unique,X_val_flavors_unique,Y_train,Y_val = train_test_split(X_train_raw,Y_train_raw,test_size=predict_days-1,align='right')
+
+
 def new_feature(X_train,Y_train,X_test,X_val=None,return_validation_score=False):
     return Y_train[-1]
     scores = []
@@ -444,6 +439,7 @@ def merge(ecs_logs,flavors_unique,training_start_time,training_end_time,predict_
     # predict_method = ridge_full
     predict_method = corrcoef_supoort_ridge
     # predict_method = features_ridge
+    features_building(ecs_logs,flavors_unique,training_start_time,training_end_time,predict_start_time,predict_end_time)
 
     return predict_method(ecs_logs,flavors_unique,training_start_time,training_end_time,predict_start_time,predict_end_time)
 
