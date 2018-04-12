fd6577e77fe8d883bbcb5f71bd753b0fd3f53328
taita2015 <mmix.xia@gmail.com>
2018-04-12 10:56:03 +0800

    0412

--- src/ecs/predictor.py
+++ src/ecs/predictor.py
@@ -73,14 +73,14 @@ def resampling(ecs_logs,flavors_unique,training_start_time,predict_start_time,fr
 # sparse feature--  dense feature
 
 # fix griding bug @2018-04-12
-def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time,training_end_time,predict_start_time,predict_end_time,outlier_handeling=True):
+def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time,training_end_time,predict_start_time,predict_end_time):
     mapping_index = get_flavors_unique_mapping(flavors_unique)
     predict_days = (predict_end_time-predict_start_time).days
 
     strike = 1
     sample = resampling(ecs_logs,flavors_unique,training_start_time,predict_start_time,frequency=predict_days,strike=strike,skip=0)
 
-    def outlier_handling(sample,method='mean',max_sigma=3):
+    def outlier_handling(sample,method='mean',max_sigma=4):
         assert(method=='mean' or method=='zero')
         sample = matrix_copy(sample)
         std_ = stdev(sample)
@@ -94,15 +94,12 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
                         sample[i][j] = 0
         return sample
 
-
-    if outlier_handeling:
-        sample = outlier_handling(sample,method='mean')
+    sample = outlier_handling(sample,method='mean')
     Ys = sample[1:]
 
     def flavor_clustering(sample,k=3,variance_threshold=None):
         corrcoef_sample = corrcoef(sample)
         clustering_paths = []
-        coef_sample = []
         for i in range(shape(sample)[1]):
             col = corrcoef_sample[i]
             col_index_sorted = argsort(col)[::-1]
@@ -110,19 +107,16 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
                 index = [i  for i in col_index_sorted if col[i]>variance_threshold]
             else:
                 index = col_index_sorted[1:k+1]
-            coef = fancy(col,index)
-            coef_sample.append(coef)
-
             clustering_paths.append(index)
-        return clustering_paths,coef_sample
+        return clustering_paths,corrcoef_sample
 
-    clustering_paths,coef_sample = flavor_clustering(sample,variance_threshold=0.5)
+    clustering_paths,coef_sample = flavor_clustering(sample,variance_threshold=0.3)
     # clustering_paths contians 0 index 
 
 
-    def get_feature_grid(coef_sample,i,fill_na='mean',max_na_rate=1,col_count=None,with_test=True):
+    def get_feature_grid(sample,i,fill_na='mean',max_na_rate=1,col_count=None,with_test=True):
         assert(fill_na=='mean' or fill_na=='zero')
-        col = fancy(coef_sample,None,i)
+        col = fancy(sample,None,i)
         R = []
         for j in range(len(col)):
             left = [None for _ in range(len(col)-j)]
@@ -154,7 +148,7 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
                         if fill_na=='mean':
                             R[_][j] = means[j]
                         elif fill_na=='zero':
-                            R[_][j]==0
+                            R[_][j]=0
         if with_test:
             if col_count!=None:
                 return fancy(R,None,(-col_count,))
@@ -162,7 +156,7 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
                 return R
         else:
             if col_count!=None:
-                return fancy(R,None,(-col_count,))
+                return fancy(R,(0,-1),(-col_count,))
             else:            
                 return R[:-1]
 
@@ -173,7 +167,8 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
         pass
     def get_mem_rate_X(sample,i):
         pass
-
+    def get_accumulate_X(sample,i):
+        pass
     #     sum_ = sum(X,axis=1)
     #     return [X[i] if sum_[i]==0 else multiply(X[i],1/float(sum_[i])) for i in range(shape(X)[0])]
     # rate_X = get_rate_X(X)
@@ -181,22 +176,23 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
     #     cpu_config,mem_config = get_machine_config(flavors_unique)
     #     return X
 
-
     X_trainS,Y_trainS,X_test_S = [],[],[]
 
     for f in flavors_unique:
-        fill_na = 'mean'
-        max_na_rate = 0.7
         # 1. get training grid
-        X = get_feature_grid(sample,mapping_index[f],col_count=None,fill_na=fill_na,max_na_rate=max_na_rate,with_test=True)
+        X = get_feature_grid(sample,mapping_index[f],col_count=None,fill_na='mean',max_na_rate=0.5,with_test=True)
         X_test = X[-1:]
         X = X[:-1]
         y = fancy(Ys,None,mapping_index[f])
         # 2.use clustering data,or not
-        # clustering_path_f = clustering_paths[mapping_index[f]]
-        # for p in clustering_path_f:
-        #     X.extend(get_feature_grid(sample,p,fill_na=fill_na,max_na_rate=max_na_rate,with_test=False))
-        #     y.extend(fancy(Ys,None,p))
+        clustering_path_f = clustering_paths[mapping_index[f]]
+        for p in clustering_path_f:
+            __x = get_feature_grid(sample,p,col_count=None,fill_na='mean',max_na_rate=0.5,with_test=False)
+            # __x = multiply(__x,coef_sample[mapping_index[f]][p]) 
+            X.extend(__x)
+            __y = fancy(Ys,None,p)
+            # __y = multiply(__y,coef_sample[mapping_index[f]][p]) 
+            y.extend(__y)
         X.extend(X_test)
 
         # 3.feature eningeering
@@ -207,8 +203,8 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
         X_cube = apply(X,lambda x:math.pow(x,3))
 
         # add features
-        add_list= [X] #	77.804
-        add_list.extend([X_sqrt,X_square,X_cube])
+        add_list= [X]
+        add_list.extend([X_log1p,X_sqrt,X_square,X_cube])
         X = hstack(add_list)
         
         # # ---------------------------------------------
@@ -217,19 +213,13 @@ def features_building(ecs_logs,flavors_config,flavors_unique,training_start_time
         # X = minmax_scaling(X) 
         X = maxabs_scaling(X)
         # X = standard_scaling(X)
-
         assert(shape(X)[0]==shape(y)[0]+1)
         X_trainS.append(X[:-1])
         X_test_S.append(X[-1:])
         Y_trainS.append(y)
     return X_trainS,Y_trainS,X_test_S
 
-
-# code backup..
-# nclf = Ridge(alpha=2,fit_intercept=True,bias_penalty=True)
-# nclf = Dynamic_KNN_Regressor(k=3,verbose=False)
-# nclf = grid_search_cv(KNN_Regressor,{'k':[3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],'verbose':[True]},nx,ny,verbose=True,is_shuffle=False,scoring='loss')
-
+# RRE todo
 def merge(ecs_logs,flavors_config,flavors_unique,training_start_time,training_end_time,predict_start_time,predict_end_time,verbose = True):
     predict = {}.fromkeys(flavors_unique)
     for f in flavors_unique:
@@ -258,15 +248,14 @@ def merge(ecs_logs,flavors_config,flavors_unique,training_start_time,training_en
         y = Y_trainS[mapping_index[f]]
         X_test = X_testS[mapping_index[f]]
         # clf = Ridge(alpha=2,fit_intercept=True)
-        clf = Ridge(alpha=1,fit_intercept=True)
+        # clf = Ridge(alpha=1,fit_intercept=True)
         # clf = grid_search_cv(Ridge,{'alpha':[0.1,1,2,4],'fit_intercept':[True]},X,y,verbose=True,is_shuffle=False,scoring='score')
-        # clf = bagging_estimator(Ridge,{'alpha':1,"fit_intercept":True},max_clf=10)
-        from ensemble import bagging_with_model
-        X_,y_ = bagging_with_model(clf,X,y,X,y)
-        # clf = KNN_Regressor(k=4,verbose=True)
-        # clf = grid_search_cv(KNN_Regressor,{'k':[3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]},X,y,verbose=True,is_shuffle=False,scoring='loss')
-        # clf = Dynamic_KNN_Regressor(k=4,verbose=False)
-        clf.fit(X_,y_)
+        clf = bagging_estimator(Ridge,{'alpha':0.1,"fit_intercept":True},max_clf=10)
+        # clf = bagging_estimator(Dynamic_KNN_Regressor,{'k':2},max_clf=100)
+        # clf = KNN_Regressor(k=10,verbose=True)
+        # clf = grid_search_cv(Dynamic_KNN_Regressor,{'k':[2,3,4,5,6,7,8,9,10]},X,y,verbose=True,is_shuffle=False,scoring='score')
+        # clf = Dynamic_KNN_Regressor(k=3,verbose=False)
+        clf.fit(X,y)
         clfs.append(clf)
 
     val_y = []
@@ -307,8 +296,6 @@ def merge(ecs_logs,flavors_config,flavors_unique,training_start_time,training_en
         predict[f] = int(round(p))
         virtual_machine_sum += int(round(p))
     return predict,virtual_machine_sum
-
-
 
 
 
